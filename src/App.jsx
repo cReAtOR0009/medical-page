9ed5ab6d3989f2e47ad65b66547ba4a79e15483a
cReAtOR0009 <epidnugoairdrop@gmail.com>
2024-10-17 19:09:56 +0100

    setup dna model

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React, { Suspense, useState } from "react";
 import { Canvas, useFrame } from "@react-three/fiber";
-import { Box, Capsule, Environment, OrbitControls } from "@react-three/drei";
+import { Box, Capsule, Environment, OrbitControls, MeshTransmissionMaterial, useFBO } from "@react-three/drei";
+import Dna3d  from "./dna"
 
 function Loading() {
   return
@@ -10,18 +11,21 @@ function Loading() {
     </div>
 }
 function App() {
+
+ 
   return (
     <Canvas>
       <color args={["#d1e2ef"]} attach="background" />
 
-      <Capsule args={[1,2,4,32]}>
-        <meshPhysicalMaterial color={"#d1e2ef"} />
+      <Capsule args={[0.9,2.5,4,32]}>
+        {/* <meshPhysicalMaterial color={"#d1e2ef"} /> */}
+        <MeshTransmissionMaterial thickness={0.1} anisotropy={2} chromaticAberration={0.3}/>
+      {/* <Box /> */}
       </Capsule>
-      
       <Suspense fallback={<Loading />}>
+      <Dna3d scale={'0.1'} position={[0, -1.6, -0.05]}/>
         <Environment preset="city" />
       </Suspense>
-      <Box />
       <OrbitControls />
     </Canvas>
   );
